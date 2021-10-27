79ca6e611b9086ea51f92ebedaa69750d282cb46
Kim HyunWoo <stories2@naver.com>
2021-10-28 00:28:22 +0900

    Update test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,3 +1,6 @@
+import pytest
+
+@pytest.mark.run(order=1)
 def test_version(db_engine):
     result = db_engine.execute("select version();")
     assert result.fetchone()[0] == "10.5.12-MariaDB-1:10.5.12+maria~focal"
